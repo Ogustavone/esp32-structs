1be5237e5d475e9bdaf5490d2d86d0f2172e46d5
Gustavo S. <ogustavone@gmail.com>
2024-10-29 16:29:38 -0300

    Update main.c
    Structs definidas, setup criado

--- main.c
+++ main.c
@@ -3,3 +3,31 @@
 
 #include <stdio.h>
 #include <unistd.h>
+
+// número do pino: int, nome: char
+struct pins {
+  int pin;
+  char name[5];
+};
+
+// Array de leds usando a struct anterior
+struct pins leds[4] = {
+  {14, "led1"},
+  {13, "led2"},
+  {12, "led3"},
+  {11, "led4"},
+};
+
+// Inicialização (setup pinmode: output) - Arduino
+void setup() {
+  Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    Serial.println("Adicionando" + String(leds[i].name) + "no pino" + String(leds[i].pin));
+    pinMode(leds[i].pin, OUTPUT);
+  }
+};
+
+// Função loop - Arduino
+void loop() {
+
+}
